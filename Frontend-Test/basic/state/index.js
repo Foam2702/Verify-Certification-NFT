2d06fa53c7f84bd78dff2f3469d4fb6a561cd29e
VietPham <bphm82@gmail.com>
2024-04-03 23:42:15 +0700

    call addVerifier successfully. Try isVerifier

--- Frontend-Test/basic/state/index.js
+++ Frontend-Test/basic/state/index.js
@@ -1,13 +1,32 @@
 import SOUNDBOUND from "../../artifacts/contracts/SoundboundToken.sol/SoundboundToken.json" with { type: "json" }
+let account;
+const web3 = new Web3("https://sepolia.infura.io/v3/9522458e32254478965be26d71bfaf88");
 
-// web3 lib instance
-const web3 = new Web3(window.ethereum);
-const contractAddress = '0xED877A7B3c30ed50e983b7B9a26524C1C4c0eB02'; // Thay YOUR_CONTRACT_ADDRESS bằng địa chỉ của smart contract Solidity của bạn
-const contract = new web3.eth.Contract(SOUNDBOUND.abi, contractAddress);
+const verifierAddress = '0x0f670Fdb84de5356B14000297668be50675A79eA'
+const organizationCode = 'IIG'
+
 async function logTicket() {
-    const response = await fetch("http://localhost:3001/tickets")
-        .then((res) => res.json());
-    console.log(response);
+    await connectMetaMask();
+    // const response = await fetch("http://localhost:3001/tickets").then((res) => res.json());
+    // console.log("ACCOUNT", account)
+    // console.log(response);
+    const contract = new web3.eth.Contract(SOUNDBOUND.abi, account);
+    // contract.methods.addVerifier(verifierAddress, organizationCode)
+    //     .send({ from: account, gas: 500000 })
+    //     .then(receipt => {
+    //         console.log('Verifier added successfully:', receipt);
+    //     })
+    //     .catch(error => {
+    //         console.error('Error adding verifier:', error);
+    //     });
+    contract.methods.isVerifier(verifierAddress)
+        .call()
+        .then(isVerifier => {
+            console.log('Is verifier:', isVerifier);
+        })
+        .catch(error => {
+            console.error('Error checking verifier:', error);
+        });
 }
 
 async function signData() {
@@ -24,24 +43,19 @@ async function recoverAddress() {
     const recoveredAddress = await web3.eth.personal.ecRecover(data, signature);
     document.getElementById("recoveredAddress").value = recoveredAddress;
 }
+async function connectMetaMask() {
+    ethereum.request({ method: 'eth_requestAccounts' }).then(accounts => {
+        account = accounts[0];
+        ethereum.request({ method: 'eth_getBalance', params: [account, 'latest'] }).then(result => {
+            let wei = parseInt(result, 16);
+            let balance = wei / (10 ** 18);
+        })
+    })
+}
 document.getElementById('sign').addEventListener('click', signData)
 document.getElementById('recover').addEventListener('click', recoverAddress)
 document.getElementById('fetchTicket').addEventListener("click", logTicket);
-document.getElementById('connect-button').addEventListener('click', event => {
-    let account;
-    let button = event.target
-    ethereum.request({ method: 'eth_requestAccounts' }).then(accounts => {
-        account = accounts[0];
-        console.log(account)
-        button.textContent = account;
-        ethereum.request({ method: 'eth_getBalance', params: [account, 'latest'] }).then(result => {
-            console.log(result)
-            let wei = parseInt(result, 16);
-            let balance = wei / (10 ** 18);
-            console.log(balance + "ETH")
-        })
-    })
-})
+document.getElementById('connect-button').addEventListener('click', connectMetaMask)
 
 
 

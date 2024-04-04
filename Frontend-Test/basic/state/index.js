be4bbffecace7f3cad6d8a26d0dfcce410b70824
VietPham <bphm82@gmail.com>
2024-04-04 17:15:01 +0700

    deploy smart contract successfully and use addVerifier, isVerifier, getOrgCode

--- Frontend-Test/basic/state/index.js
+++ Frontend-Test/basic/state/index.js
@@ -1,25 +1,35 @@
 import SOULBOUND from "../../artifacts/contracts/SoulboundToken.sol/SoulboundToken.json" with { type: "json" }
-let account;
-const web3 = new Web3("https://sepolia.infura.io/v3/9522458e32254478965be26d71bfaf88");
+//import SOULBOUND_ADDRESS from "../../src/state/nft-market/config.ts"with { type: "json" }
+const SOULBOUND_ADDRESS = "0xD628ce6a8CF8754083bD05b07A096D38e04465f2"
 
-const verifierAddress = '0x0f670Fdb84de5356B14000297668be50675A79eA'
-const organizationCode = 'IIG'
+let account;
+const web3 = new Web3(window.ethereum);
+const contract = new web3.eth.Contract(SOULBOUND.abi, SOULBOUND_ADDRESS);
 
 async function logTicket() {
     await connectMetaMask();
-    // const response = await fetch("http://localhost:3001/tickets").then((res) => res.json());
-    // console.log("ACCOUNT", account)
-    // console.log(response);
-    const contract = new web3.eth.Contract(SOULBOUND.abi, account);
-    // contract.methods.addVerifier(verifierAddress, organizationCode)
-    //     .send({ from: account, gas: 500000 })
-    //     .then(receipt => {
-    //         console.log('Verifier added successfully:', receipt);
-    //     })
-    //     .catch(error => {
-    //         console.error('Error adding verifier:', error);
-    //     });
-    contract.methods.isVerifier(verifierAddress)
+    const response = await fetch("http://localhost:3001/tickets").then((res) => res.json());
+    console.log("ACCOUNT", account)
+    console.log(response);
+}
+async function addVerifier() {
+    await connectMetaMask();
+    const addr = document.getElementById("address_verfifier").value
+    const org = document.getElementById("organization").value
+
+    contract.methods.addVerifier(addr, org)
+        .send({ from: account, gas: 500000 })
+        .then(receipt => {
+            console.log('Verifier added successfully:', receipt);
+        })
+        .catch(error => {
+            console.error('Error adding verifier:', error);
+        });
+}
+async function isVerifier() {
+    await connectMetaMask();
+    const addr = document.getElementById("is_verifier").value;
+    contract.methods.isVerifier(addr)
         .call()
         .then(isVerifier => {
             console.log('Is verifier:', isVerifier);
@@ -27,6 +37,19 @@ async function logTicket() {
         .catch(error => {
             console.error('Error checking verifier:', error);
         });
+}
+async function getOrganizationCode() {
+    await connectMetaMask();
+    const org = document.getElementById("org_code").value;
+    console.log(org)
+    contract.methods.getOrganizationCode(org)
+        .call((error, result) => {
+            if (!error) {
+                console.log("Organization code: ", result);
+            } else {
+                console.error("Error:", error);
+            }
+        })
 }
 
 async function signData() {
@@ -52,10 +75,14 @@ async function connectMetaMask() {
         })
     })
 }
+
 document.getElementById('sign').addEventListener('click', signData)
 document.getElementById('recover').addEventListener('click', recoverAddress)
-document.getElementById('fetchTicket').addEventListener("click", logTicket);
+document.getElementById('fetch_ticket').addEventListener("click", logTicket);
 document.getElementById('connect-button').addEventListener('click', connectMetaMask)
+document.getElementById('addVerifier').addEventListener("click", addVerifier)
+document.getElementById('isVerifier').addEventListener("click", isVerifier)
+document.getElementById('getOrganizationCode').addEventListener("click", getOrganizationCode)
 
 
 

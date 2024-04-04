52355ab9f3d921c06bab9a126a2abe4fece0a66e
VietPham <bphm82@gmail.com>
2024-04-04 15:15:07 +0700

    change soundbound to soulbound

--- SoundboundToken.sol
+++ SoundboundToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract SoundboundToken is ERC721 {
+contract SoulboundToken is ERC721 {
   address public owner;
   using Counters for Counters.Counter;
   Counters.Counter private _nSBTs;

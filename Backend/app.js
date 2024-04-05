22a5fb88fa8d1c5b229ce85a21fe52a81376917c
VietPham <bphm82@gmail.com>
2024-04-05 17:24:14 +0700

    trying socket

--- Backend/app.js
+++ Backend/app.js
@@ -1,16 +1,29 @@
 const express = require("express")
-const app = express();
-const PORT = 3001;
 const route = require("./src/routes/index")
 const cors = require("cors")
+const { Server } = require('socket.io')
+const { createServer } = require('node:http')
+const { join } = require('node:path')
+const app = express();
+const PORT = 3001;
+const server = createServer(app)
+const io = new Server(server, {
+    connectionStateRecovery: {}
+})
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
-
-
-route(app);
-app.listen(PORT, () => {
-    console.log(`Starting at port:${PORT}`)
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'))
 })
 
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+//route(app);
+server.listen(PORT, () => {
+    console.log(`Starting at port:${PORT}`)
+})
 

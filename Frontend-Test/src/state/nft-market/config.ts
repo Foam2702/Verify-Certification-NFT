aa021455953811bdc9085173b0ee486deb07c4a1
VietPham <bphm82@gmail.com>
2024-04-09 14:23:12 +0700

    create Dapp

--- Frontend-Test/src/state/nft-market/config.ts
+++
@@ -1,2 +0,0 @@
-export const SOULBOUND_ADDRESS = process.env
-  .SOULBOUND_ADDRESS as string;
--- Frontend-Test/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ["./src/**/*.tsx", "./src/styles/*"],
-  theme: {
-    extend: {},
-  },
-
-  plugins: [],
-};

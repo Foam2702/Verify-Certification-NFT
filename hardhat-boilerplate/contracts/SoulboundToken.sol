2c8b7954eb184549da43e2fd0d5c46fd87d614c9
VietPham <bphm82@gmail.com>
2024-04-10 10:37:19 +0700

    get issuer by organization code

--- hardhat-boilerplate/contracts/SoulboundToken.sol
+++ hardhat-boilerplate/contracts/SoulboundToken.sol
@@ -1,185 +1,184 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.3;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract SoulboundToken is ERC721 {
-  address public owner;
-  using Counters for Counters.Counter;
-  Counters.Counter private _nSBTs;
-  Counters.Counter private _nVerifiers;
+    address public owner;
+    using Counters for Counters.Counter;
+    Counters.Counter private _nSBTs;
 
-  struct SBTInfo {
-    uint256 sBTId;      // ID của SBT
-    string fullName;     // Họ và tên của người dùng
-    string certificateHash; // Mã hash của hình ảnh chứng chỉ trên IPFS
-    string citizenID;    // Số chứng minh nhân dân (CCCD) của người dùng
-    string hometown;     // Quê quán của người dùng
-    string nationality;   // Quốc tịch của người dùng
-    uint256 birthDate;    // Ngày sinh của người dùng 
-    address ownerAddress;  // Địa chỉ chủ sở hữu của token
-  }
-
-  struct VerifierInfo {
-    bool isVerifier;      // Có phải là người xác thực không 
-    string organizationCode;  // Mã tổ chức
-  }
-
-  // Liên kết thông tin của SBT với ID
-  mapping(uint256 => SBTInfo) public idToSBT;
-
-  // Liên kết thông tin của verifierInfor với verifierList
-  mapping(address => VerifierInfo) public verifierList;
-
-  // Khai báo modifier `onlyOwner` để chỉ cho phép chủ sở hữu hợp đồng thực hiện các hành động được quy định
-  constructor() ERC721("SoulboundToken", "SBT") {
-    owner = msg.sender;
-  }
-   
-  // Hàm thêm một địa chỉ vào danh sách các địa chỉ được phép xác thực
-  function addVerifier(address verifier, string memory organizationCode) public {
-    verifierList[verifier] = VerifierInfo(true, organizationCode);
-  }
-  // Hàm lấy thông tin địa chỉ và mã tổ chức của tất của verifier
-  function getVerifierList() public view returns (address[] memory, string[] memory) {
-      // Khởi tạo mảng tạm thời để lưu trữ địa chỉ và mã tổ chức
-      address[] memory addresses = new address[](_nVerifiers.current());
-      string[] memory organizationCodes = new string[](_nVerifiers.current());
-
-      // Lặp qua mapping để lấy địa chỉ và mã tổ chức của các verifier
-      uint index = 0;
-      for (uint i = 0; i < _nVerifiers.current(); i++) {
-          address verifierAddress = address(uint160(i)); // Chuyển đổi số nguyên thành địa chỉ
-          if (verifierList[verifierAddress].isVerifier) {
-              addresses[index] = verifierAddress;
-              organizationCodes[index] = verifierList[verifierAddress].organizationCode;
-              index++;
-          }
-      }
-
-      // Khởi tạo mảng kết quả với kích thước chính xác
-      address[] memory resultAddresses = new address[](index);
-      string[] memory resultOrganizationCodes = new string[](index);
-      for (uint i = 0; i < index; i++) {
-          resultAddresses[i] = addresses[i];
-          resultOrganizationCodes[i] = organizationCodes[i];
-      }
-
-      return (resultAddresses, resultOrganizationCodes);
-  }
-   
-  // Hàm kiểm tra xem một địa chỉ có trong danh sách xác thực hay không
-  function isVerifier(address addr) public view returns (bool) {
-    return verifierList[addr].isVerifier;
-  }
-   
-  // Hàm lấy mã tổ chức của một địa chỉ xác thực
-  function getOrganizationCode(address addr) public view returns (string memory) {
-    return verifierList[addr].organizationCode;
-  }
-   
-  // Hàm phục hồi địa chỉ người ký
-  function recover(bytes32 _ethSignedMessageHash, bytes memory _sig) public pure returns (address) {
-    (bytes32 r, bytes32 s, uint8 v) = _split(_sig);
-    return ecrecover(_ethSignedMessageHash, v, r, s);
-  }
-   
-  // Hàm xác định các thành phần r,s,v trong 1 chữ ký
-  function _split(bytes memory _sig) internal pure returns (bytes32 r, bytes32 s, uint8 v) {
-    require(_sig.length == 65, "invalid signature length");
-    assembly {
-      r := mload(add(_sig, 32))
-      s := mload(add(_sig, 64))
-      v := byte(0, mload(add(_sig, 96)))
+    struct SBTInfo {
+        uint256 sBTId;          // ID của SBT
+        string fullName;        // Họ và tên của người dùng
+        string certificateHash; // Mã hash của hình ảnh chứng chỉ trên IPFS
+        string citizenID;       // Số chứng minh nhân dân (CCCD) của người dùng
+        string hometown;        // Quê quán của người dùng
+        string nationality;     // Quốc tịch của người dùng
+        uint256 birthDate;      // Ngày sinh của người dùng 
+        address ownerAddress;   // Địa chỉ chủ sở hữu của token
     }
-  }
-   
-  // Hàm trả về mảng các SBT thuộc sở hữu của người gọi hàm
-  function getMySBTs() public view returns (SBTInfo[] memory) {
-    uint totalSBTCount = _nSBTs.current();
-    uint sbtCount = 0;
-     
-    // Đếm số lượng SBT mà người dùng hiện tại sở hữu
-    for (uint i = 1; i <= totalSBTCount; i++) {
-      if (idToSBT[i].ownerAddress == msg.sender) {
-        sbtCount += 1;
+
+    struct VerifierInfo {
+        bool isVerifier;         // Có phải là người xác thực không 
+        string organizationCode; // Mã tổ chức
+    }
+
+    // Liên kết thông tin của SBT với ID
+    mapping(uint256 => SBTInfo) public idToSBT;
+
+    // Liên kết thông tin của verifierInfo với verifierList
+    mapping(address => VerifierInfo) public verifierList;
+    address[] public verifierAddresses;
+    // Khai báo modifier `onlyOwner` để chỉ cho phép chủ sở hữu hợp đồng thực hiện các hành động được quy định
+    constructor() ERC721("SoulboundToken", "SBT") {
+        owner = msg.sender;
+    }
+
+    // Hàm thêm một địa chỉ vào danh sách các địa chỉ được phép xác thực
+    function addVerifier(address verifier, string memory organizationCode) public {
+        verifierList[verifier] = VerifierInfo(true, organizationCode);
+        verifierAddresses.push(verifier);
+    }
+
+    // Hàm kiểm tra xem một địa chỉ có trong danh sách xác thực hay không
+    function isVerifier(address addr) public view returns (bool) {
+        return verifierList[addr].isVerifier;
+    }
+
+    // Hàm lấy mã tổ chức của một địa chỉ xác thực
+    function getOrganizationCode(address addr) public view returns (string memory) {
+        return verifierList[addr].organizationCode;
+    }
+     // Hàm phục hồi địa chỉ người ký
+    function recover(bytes32 _ethSignedMessageHash, bytes memory _sig) public pure returns (address) {
+      (bytes32 r, bytes32 s, uint8 v) = _split(_sig);
+      return ecrecover(_ethSignedMessageHash, v, r, s);
+    }
+    
+    // Hàm xác định các thành phần r,s,v trong 1 chữ ký
+    function _split(bytes memory _sig) internal pure returns (bytes32 r, bytes32 s, uint8 v) {
+      require(_sig.length == 65, "invalid signature length");
+      assembly {
+        r := mload(add(_sig, 32))
+        s := mload(add(_sig, 64))
+        v := byte(0, mload(add(_sig, 96)))
       }
     }
-     
-    // Tạo mảng để lưu trữ thông tin về SBT của người dùng hiện tại
-    SBTInfo[] memory sbts = new SBTInfo[](sbtCount);
-    uint currentIndex = 0;
-     
-    // Lặp lại tất cả SBT và lưu trữ thông tin về SBT của người dùng hiện tại vào mảng
-    for (uint i = 1; i <= totalSBTCount; i++) {
-      if (idToSBT[i].ownerAddress == msg.sender) {
-        sbts[currentIndex] = idToSBT[i];
-        currentIndex += 1;
+    
+    // Hàm trả về mảng các SBT thuộc sở hữu của người gọi hàm
+    function getMySBTs() public view returns (SBTInfo[] memory) {
+      uint totalSBTCount = _nSBTs.current();
+      uint sbtCount = 0;
+      
+      // Đếm số lượng SBT mà người dùng hiện tại sở hữu
+      for (uint i = 1; i <= totalSBTCount; i++) {
+        if (idToSBT[i].ownerAddress == msg.sender) {
+          sbtCount += 1;
+        }
       }
+      
+      // Tạo mảng để lưu trữ thông tin về SBT của người dùng hiện tại
+      SBTInfo[] memory sbts = new SBTInfo[](sbtCount);
+      uint currentIndex = 0;
+      
+      // Lặp lại tất cả SBT và lưu trữ thông tin về SBT của người dùng hiện tại vào mảng
+      for (uint i = 1; i <= totalSBTCount; i++) {
+        if (idToSBT[i].ownerAddress == msg.sender) {
+          sbts[currentIndex] = idToSBT[i];
+          currentIndex += 1;
+        }
+      }
+      
+      return sbts;
     }
-     
-    return sbts;
-  }
-   
-  //hàm riêng để thiết lập thông tin 
-  function setSBTInfo(
+    
+    //hàm riêng để thiết lập thông tin 
+    function setSBTInfo(
+        string memory fullName,
+        string memory certificateHash,
+        string memory citizenID,
+        string memory hometown,
+        string memory nationality,
+        uint256 birthDate
+    ) internal pure returns (SBTInfo memory) {
+        return SBTInfo({
+            fullName: fullName,
+            certificateHash: certificateHash,
+            citizenID: citizenID,
+            hometown: hometown,
+            nationality: nationality,
+            birthDate: birthDate,
+            sBTId: 0, // Khởi tạo sBTId với giá trị mặc định
+            ownerAddress: address(0) // Khởi tạo ownerAddress với giá trị mặc định
+        });
+    }
+
+    // Hàm tạo một SBT mới cho một địa chỉ.
+
+    function mintSBTForAddress(
+      address recipient,
       string memory fullName,
       string memory certificateHash,
       string memory citizenID,
       string memory hometown,
       string memory nationality,
       uint256 birthDate
-  ) internal pure returns (SBTInfo memory) {
-      return SBTInfo({
-          fullName: fullName,
-          certificateHash: certificateHash,
-          citizenID: citizenID,
-          hometown: hometown,
-          nationality: nationality,
-          birthDate: birthDate,
-          sBTId: 0, // Khởi tạo sBTId với giá trị mặc định
-          ownerAddress: address(0) // Khởi tạo ownerAddress với giá trị mặc định
-      });
-  }
+    ) public {
+      require(msg.sender == owner, "Only the owner can mint SBTs for addresses");
 
-  // Hàm tạo một SBT mới cho một địa chỉ.
+      _nSBTs.increment(); // Tăng ID SBT
+      uint256 newSBTId = _nSBTs.current(); // Lấy ID của SBT mới
 
-  function mintSBTForAddress(
-    address recipient,
-    string memory fullName,
-    string memory certificateHash,
-    string memory citizenID,
-    string memory hometown,
-    string memory nationality,
-    uint256 birthDate
-  ) public {
-    require(msg.sender == owner, "Only the owner can mint SBTs for addresses");
+      // Gọi hàm setSBTInfo để tạo một đối tượng SBTInfo mới
+      SBTInfo memory newSBTInfo = setSBTInfo(
+        fullName,
+        certificateHash,
+        citizenID,
+        hometown,
+        nationality,
+        birthDate
+      );
+      
+      // Cập nhật thông tin cho SBT mới
+      newSBTInfo.sBTId = newSBTId;
+      newSBTInfo.ownerAddress = recipient;
+      
+      // Lưu thông tin SBT vào mapping
+      idToSBT[newSBTId] = newSBTInfo;
 
-    _nSBTs.increment(); // Tăng ID SBT
-    uint256 newSBTId = _nSBTs.current(); // Lấy ID của SBT mới
+      // Gọi hàm `_safeMint` để tạo NFT cho địa chỉ nhận
+      _safeMint(recipient, newSBTId);
+    }
 
-    // Gọi hàm setSBTInfo để tạo một đối tượng SBTInfo mới
-    SBTInfo memory newSBTInfo = setSBTInfo(
-      fullName,
-      certificateHash,
-      citizenID,
-      hometown,
-      nationality,
-      birthDate
-    );
-    
-    // Cập nhật thông tin cho SBT mới
-    newSBTInfo.sBTId = newSBTId;
-    newSBTInfo.ownerAddress = recipient;
-    
-    // Lưu thông tin SBT vào mapping
-    idToSBT[newSBTId] = newSBTInfo;
+    // Hàm tìm các địa chỉ có mã tổ chức cụ thể
+    function getVerifiersByOrganizationCode(string memory targetOrganizationCode) public view returns (address[] memory) {
+        uint verifierCount = 0;
+        address[] memory result;
 
-    // Gọi hàm `_safeMint` để tạo NFT cho địa chỉ nhận
-    _safeMint(recipient, newSBTId);
-  }
+        // Duyệt qua toàn bộ danh sách các địa chỉ verifier
+        for (uint i = 0; i < verifierAddresses.length; i++) {
+            address verifierAddress = verifierAddresses[i];
+            if (keccak256(bytes(verifierList[verifierAddress].organizationCode)) == keccak256(bytes(targetOrganizationCode))) {
+                verifierCount++;
+            }
+        }
 
-  
+        // Khởi tạo mảng để lưu trữ địa chỉ của các verifier có mã tổ chức trùng khớp
+        result = new address[](verifierCount);
+        uint currentIndex = 0;
+
+        // Duyệt lại danh sách và lưu trữ địa chỉ của các verifier có mã tổ chức trùng khớp
+        for (uint i = 0; i < verifierAddresses.length; i++) {
+            address verifierAddress = verifierAddresses[i];
+            if (keccak256(bytes(verifierList[verifierAddress].organizationCode)) == keccak256(bytes(targetOrganizationCode))) {
+                result[currentIndex] = verifierAddress;
+                currentIndex++;
+            }
+        }
+
+        return result;
+    }
 }
--- hardhat-boilerplate/frontend/src/App.js
+++ hardhat-boilerplate/frontend/src/App.js
@@ -1,178 +1,178 @@
-// import React, { useState } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-// import SOULBOUND from '../src/artifacts/contracts/SoulboundToken.sol/SoulboundToken.json'
-// const { ethers } = require("ethers");
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SOULBOUND from '../src/artifacts/contracts/SoulboundToken.sol/SoulboundToken.json'
+const { ethers } = require("ethers");
 
-// const SOULBOUND_ADDRESS = process.env.REACT_APP_SOULBOUND_ADDRESS
-// function App() {
-//     const [currentAccount, setCurrentAccount] = useState(null)
-//     const [formData, setFormData] = useState({
-//         dataSign: "",
-//         signature: "",
-//         recoveredAddress: "",
-//         verifier: "",
-//         organization: "",
-//         isverifier: ""
-//     })
-//     const connectMetaMask = async () => {
-//         const { ethereum } = window
-//         if (!ethereum) {
-//             console.log("Install MetaMask")
-//         }
-//         else {
-//             console.log("Wallet exists")
-//         }
-//         const account = await ethereum.request({ method: 'eth_requestAccounts' })
-//         setCurrentAccount(account[0])
-//     }
+const SOULBOUND_ADDRESS = process.env.REACT_APP_SOULBOUND_ADDRESS
+function App() {
+    const [currentAccount, setCurrentAccount] = useState(null)
+    const [formData, setFormData] = useState({
+        dataSign: "",
+        signature: "",
+        recoveredAddress: "",
+        verifier: "",
+        organization: "",
+        isverifier: ""
+    })
+    const connectMetaMask = async () => {
+        const { ethereum } = window
+        if (!ethereum) {
+            console.log("Install MetaMask")
+        }
+        else {
+            console.log("Wallet exists")
+        }
+        const account = await ethereum.request({ method: 'eth_requestAccounts' })
+        setCurrentAccount(account[0])
+    }
 
-//     const logTicket = async () => {
-//         await connectMetaMask();
-//         const response = await fetch("http://localhost:3001/tickets").then((res) => res.json());
-//         console.log(response);
-//     }
-//     const handleSignData = async (e) => {
-//         e.preventDefault();
-//         const { ethereum } = window
-//         if (ethereum) {
-//             const provider = new ethers.providers.Web3Provider(ethereum);
-//             const signer = provider.getSigner();
-//             const contract = new ethers.Contract(SOULBOUND_ADDRESS, SOULBOUND.abi, signer);
-//             const data = e.target.elements.data.value;
-//             const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-//             const signature = await signer.signMessage(data, accounts[0]);
-//             const recoveredAddr = await ethers.utils.recoverAddress(ethers.utils.hashMessage(ethers.utils.toUtf8Bytes(data)), signature);
-//             setFormData(prevState => ({
-//                 ...prevState,
-//                 dataSign: data,
-//                 signature: signature,
-//                 recoveredAddress: recoveredAddr
+    const logTicket = async () => {
+        await connectMetaMask();
+        const response = await fetch("http://localhost:3001/tickets").then((res) => res.json());
+        console.log(response);
+    }
+    const handleSignData = async (e) => {
+        e.preventDefault();
+        const { ethereum } = window
+        if (ethereum) {
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(SOULBOUND_ADDRESS, SOULBOUND.abi, signer);
+            const data = e.target.elements.data.value;
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+            const signature = await signer.signMessage(data, accounts[0]);
+            const recoveredAddr = await ethers.utils.recoverAddress(ethers.utils.hashMessage(ethers.utils.toUtf8Bytes(data)), signature);
+            setFormData(prevState => ({
+                ...prevState,
+                dataSign: data,
+                signature: signature,
+                recoveredAddress: recoveredAddr
 
-//             }))
-//         }
-//     }
-//     const handleAddverifier = async (e) => {
-//         e.preventDefault()
-//         const { ethereum } = window
-//         if (ethereum) {
-//             const provider = new ethers.providers.Web3Provider(ethereum);
-//             const signer = provider.getSigner();
-//             const contract = new ethers.Contract(SOULBOUND_ADDRESS, SOULBOUND.abi, signer);
-//             const addr = e.target.elements.verifier.value;
-//             const org = e.target.elements.organization.value;
-//             const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-//             const tx = await contract.addVerifier(addr, org)
-//             await tx.wait();
-//             console.log("Verifier added successfully.");
+            }))
+        }
+    }
+    const handleAddverifier = async (e) => {
+        e.preventDefault()
+        const { ethereum } = window
+        if (ethereum) {
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(SOULBOUND_ADDRESS, SOULBOUND.abi, signer);
+            const addr = e.target.elements.verifier.value;
+            const org = e.target.elements.organization.value;
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+            const tx = await contract.addVerifier(addr, org)
+            await tx.wait();
+            console.log("Verifier added successfully.");
 
-//             setFormData(prevState => ({
-//                 ...prevState,
-//                 verifier: addr,
-//                 organization: org
+            setFormData(prevState => ({
+                ...prevState,
+                verifier: addr,
+                organization: org
 
-//             }))
-//         }
-//     }
-//     const handleIsVerifier = async (e) => {
-//         e.preventDefault()
-//         const { ethereum } = window
-//         if (ethereum) {
-//             const provider = new ethers.providers.Web3Provider(ethereum);
-//             const signer = provider.getSigner();
-//             const contract = new ethers.Contract(SOULBOUND_ADDRESS, SOULBOUND.abi, signer);
-//             const addr = e.target.elements.isVerifier.value;
+            }))
+        }
+    }
+    const handleIsVerifier = async (e) => {
+        e.preventDefault()
+        const { ethereum } = window
+        if (ethereum) {
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(SOULBOUND_ADDRESS, SOULBOUND.abi, signer);
+            const addr = e.target.elements.isVerifier.value;
 
-//             const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-//             const checkVerifier = await contract.isVerifier(addr)
-//             const organization = await contract.getOrganizationCode(addr)
-//             console.log("organization:", organization)
-//             console.log(checkVerifier)
-//             const result = await contract.getVerifierList();
-//             console.log(result)
-//             // console.log('Verifier Addresses: ', result[0]);
-//             // console.log('Organization Codes: ', result[1]);
-//             setFormData(prevState => ({
-//                 ...prevState,
-//                 isverifier: checkVerifier
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+            const checkVerifier = await contract.isVerifier(addr)
+            const organization = await contract.getOrganizationCode(addr)
+            console.log("organization:", organization)
+            console.log(checkVerifier)
 
-//             }))
-//         }
-//     }
-//     const handleGetAllVerifier = async (e) => {
-//         e.preventDefault()
-//         const { ethereum } = window
-//         if (ethereum) {
-//             const provider = new ethers.providers.Web3Provider(ethereum);
-//             const signer = provider.getSigner();
-//             const contract = new ethers.Contract(SOULBOUND_ADDRESS, SOULBOUND.abi, signer);
-//             try {
-//                 const result = await contract.getVerifierList();
-//                 console.log('Verifier Addresses: ', result[0]);
-//                 console.log('Organization Codes: ', result[1]);
-//             } catch (error) {
-//                 console.error('Error:', error);
-//             }
+            setFormData(prevState => ({
+                ...prevState,
+                isverifier: checkVerifier
 
-//         }
-//     }
-//     return (
-//         <div className="App">
-//             <header className="App-header" />
-//             <button onClick={connectMetaMask}>Connect Wallet</button>
-//             {currentAccount && <p>Connected Wallet: {currentAccount}</p>}
+            }))
+        }
+    }
+    const handleGetAllVerifier = async (e) => {
+        e.preventDefault()
+        const { ethereum } = window
+        if (ethereum) {
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(SOULBOUND_ADDRESS, SOULBOUND.abi, signer);
+            try {
+                const result = await contract.getVerifiersByOrganizationCode(e.target.elements.issuer_organization.value);
+                console.log(result);
 
-//             <form onSubmit={handleSignData}>
-//                 <label >Data:</label>
-//                 <input type="text" name="data" />
-//                 {formData.signature && <p>Signature: {formData.signature}</p>}
-//                 {formData.recoveredAddress && <p>Recoverd Address: {formData.recoveredAddress}</p>}
-//                 <br />
+            } catch (error) {
+                console.error('Error:', error);
+            }
 
-//                 <button type="submit">Sign</button>
-//             </form>
-//             <form onSubmit={handleAddverifier}>
+        }
+    }
+    return (
+        <div className="App">
+            <header className="App-header" />
+            <button onClick={connectMetaMask}>Connect Wallet</button>
+            {currentAccount && <p>Connected Wallet: {currentAccount}</p>}
 
-//                 <label >Add verifier:</label>
-//                 <input type="text" name="verifier" />
-//                 <br />
-//                 <label >Organization:</label>
-//                 <input type="text" name="organization" />
-//                 <br />
-//                 <button type="submit">Add</button>
+            <form onSubmit={handleSignData}>
+                <label >Data:</label>
+                <input type="text" name="data" />
+                {formData.signature && <p>Signature: {formData.signature}</p>}
+                {formData.recoveredAddress && <p>Recoverd Address: {formData.recoveredAddress}</p>}
+                <br />
 
-//             </form>
+                <button type="submit">Sign</button>
+            </form>
+            <form onSubmit={handleAddverifier}>
 
-//             <form onSubmit={handleIsVerifier}>
-//                 <label >Address:</label>
-//                 <input type="text" name="isVerifier" />
-//                 <br />
-//                 <button type="submit">Check</button>
-//                 {formData.isverifier ? "Is Verifier:True" : "Is Verifier:False"}
+                <label >Add verifier:</label>
+                <input type="text" name="verifier" />
+                <br />
+                <label >Organization:</label>
+                <input type="text" name="organization" />
+                <br />
+                <button type="submit">Add</button>
 
-//             </form>
+            </form>
 
-//             <form onSubmit={handleGetAllVerifier}>
-//                 <button type="submit">Get</button>
+            <form onSubmit={handleIsVerifier}>
+                <label >Address:</label>
+                <input type="text" name="isVerifier" />
+                <br />
+                <button type="submit">Check</button>
+                {formData.isverifier ? "Is Verifier:True" : "Is Verifier:False"}
 
-//             </form>
-//         </div >
-//     );
-// }
+            </form>
 
-// export default App;
+            <form onSubmit={handleGetAllVerifier}>
+                <label >Get Issuer from Organization:</label>
+                <input type="text" name="issuer_organization" />
+                <br />
+                <button type="submit">Get</button>
+
+            </form>
+        </div >
+    );
+}
+
+export default App;
 
 ///////////////////////////////////////
 
-import { Routes, Route, Navigate } from "react-router-dom"
-import Chat from "./pages/Chat"
-function App() {
-    return <>
-        <Routes>
-            <Route path="/" element={<Chat />} />
-            <Route path="*" element={<Navigate to="/" />} />
+// import { Routes, Route, Navigate } from "react-router-dom"
+// import Chat from "./pages/Chat"
+// function App() {
+//     return <>
+//         <Routes>
+//             <Route path="/" element={<Chat />} />
+//             <Route path="*" element={<Navigate to="/" />} />
 
-        </Routes>
-    </>
-}
-export default App;
+//         </Routes>
+//     </>
+// }
+// export default App;

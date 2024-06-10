f48a9db59e855501db96313c2e27ce9f2100e1ed
Phạm Minh Tiến <104308969+Mystogan602@users.noreply.github.com>
2024-06-10 19:08:12 +0700

    Update SoulboundToken.sol

--- SoulboundToken.sol
+++ SoulboundToken.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts@4.7.0/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.7.0/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts@4.7.0/access/Ownable.sol";
-import "@openzeppelin/contracts@4.7.0/utils/Counters.sol"; 
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SoulboundToken is ERC721,ERC721URIStorage,Ownable {
+contract SoulboundToken is ERC721, ERC721URIStorage, ERC721Pausable, Ownable {
     using Counters for Counters.Counter;
-    
+
     Counters.Counter private _nSBTs;
 
     struct SBTInfo {
@@ -39,17 +39,11 @@ contract SoulboundToken is ERC721,ERC721URIStorage,Ownable {
     string[] private organizationCodesList;
 
     // Khai báo modifier `onlyOwner` để chỉ cho phép chủ sở hữu hợp đồng thực hiện các hành động được quy định
-    constructor() ERC721("SoulboundToken", "SBT") Ownable() {}
+    constructor() ERC721("SoulboundToken", "SBT") Ownable(msg.sender) {}
 
-
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC721URIStorage) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
-
 
     function tokenURI(uint256 tokenId) public view virtual override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
@@ -68,7 +62,7 @@ contract SoulboundToken is ERC721,ERC721URIStorage,Ownable {
     }
 
     // Hàm xóa một địa chỉ từ danh sách các địa chỉ được phép xác thực
-    function removeVerifier(address verifier) public  {
+    function removeVerifier(address verifier) public {
         require(verifierList[verifier].isVerifier, "Address is not a verifier");
 
         // Lấy mã tổ chức của verifier
@@ -123,50 +117,50 @@ contract SoulboundToken is ERC721,ERC721URIStorage,Ownable {
         return verifierList[addr].organizationCode;
     }
 
-     // Hàm phục hồi địa chỉ người ký
+    // Hàm phục hồi địa chỉ người ký
     function recover(bytes32 _ethSignedMessageHash, bytes memory _sig) public pure returns (address) {
-      (bytes32 r, bytes32 s, uint8 v) = _split(_sig);
-      return ecrecover(_ethSignedMessageHash, v, r, s);
+        (bytes32 r, bytes32 s, uint8 v) = _split(_sig);
+        return ecrecover(_ethSignedMessageHash, v, r, s);
     }
-    
+
     // Hàm xác định các thành phần r,s,v trong 1 chữ ký
     function _split(bytes memory _sig) internal pure returns (bytes32 r, bytes32 s, uint8 v) {
-      require(_sig.length == 65, "invalid signature length");
-      assembly {
-        r := mload(add(_sig, 32))
-        s := mload(add(_sig, 64))
-        v := byte(0, mload(add(_sig, 96)))
-      }
+        require(_sig.length == 65, "invalid signature length");
+        assembly {
+            r := mload(add(_sig, 32))
+            s := mload(add(_sig, 64))
+            v := byte(0, mload(add(_sig, 96)))
+        }
     }
-    
+
     // Hàm trả về mảng các SBT thuộc sở hữu của người gọi hàm
     function getMySBTs() public view returns (SBTInfo[] memory) {
-      uint totalSBTCount = _nSBTs.current();
-      uint sbtCount = 0;
-      
-      // Đếm số lượng SBT mà người dùng hiện tại sở hữu
-      for (uint i = 1; i <= totalSBTCount; i++) {
-        if (idToSBT[i].ownerAddress == msg.sender) {
-          sbtCount += 1;
+        uint totalSBTCount = _nSBTs.current();
+        uint sbtCount = 0;
+
+        // Đếm số lượng SBT mà người dùng hiện tại sở hữu
+        for (uint i = 1; i <= totalSBTCount; i++) {
+            if (idToSBT[i].ownerAddress == msg.sender) {
+                sbtCount += 1;
+            }
         }
-      }
-      
-      // Tạo mảng để lưu trữ thông tin về SBT của người dùng hiện tại
-      SBTInfo[] memory sbts = new SBTInfo[](sbtCount);
-      uint currentIndex = 0;
-      
-      // Lặp lại tất cả SBT và lưu trữ thông tin về SBT của người dùng hiện tại vào mảng
-      for (uint i = 1; i <= totalSBTCount; i++) {
-        if (idToSBT[i].ownerAddress == msg.sender) {
-          sbts[currentIndex] = idToSBT[i];
-          currentIndex += 1;
+
+        // Tạo mảng để lưu trữ thông tin về SBT của người dùng hiện tại
+        SBTInfo[] memory sbts = new SBTInfo[](sbtCount);
+        uint currentIndex = 0;
+
+        // Lặp lại tất cả SBT và lưu trữ thông tin về SBT của người dùng hiện tại vào mảng
+        for (uint i = 1; i <= totalSBTCount; i++) {
+            if (idToSBT[i].ownerAddress == msg.sender) {
+                sbts[currentIndex] = idToSBT[i];
+                currentIndex += 1;
+            }
         }
-      }
-      
-      return sbts;
+
+        return sbts;
     }
-    
-    //hàm riêng để thiết lập thông tin 
+
+    // hàm riêng để thiết lập thông tin 
     function setSBTInfo(
         string memory fullName,
         string memory certificateHash,
@@ -191,15 +185,14 @@ contract SoulboundToken is ERC721,ERC721URIStorage,Ownable {
     function mintSBTForAddress(
         address recipient,
         string memory myTokenURI
-    ) public   {
-        
+    ) public {
 
         _nSBTs.increment(); // Tăng ID SBT
         uint256 newSBTId = _nSBTs.current(); // Lấy ID của SBT mới
 
         // Gọi hàm `_safeMint` để tạo NFT cho địa chỉ nhận
         _safeMint(recipient, newSBTId);
-        
+
         // Gọi hàm `_setTokenURI` để thiết lập URI cho token mới
         _setTokenURI(newSBTId, myTokenURI);
     }
@@ -232,14 +225,19 @@ contract SoulboundToken is ERC721,ERC721URIStorage,Ownable {
 
         return result;
     }
-    // Hàm ngăn chặn chuyển token sang tài khoản khác
-    function _beforeTokenTransfer(
-        address from, 
-        address to, 
-        uint256 tokenId
-    ) internal override virtual {
-        require(from == address(0), "Err: Soulbound token transfer is BLOCKED");   
-        super._beforeTokenTransfer(from, to, tokenId);  
+
+    // Hàm ngăn chặn chuyển giao soulbound token
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Pausable)
+        returns (address)
+    {
+        address from = _ownerOf(tokenId);
+        if (from != address(0) && to != address(0)) {
+            revert("Soulbound: Transfer failed");
+        }
+
+        return super._update(to, tokenId, auth);
     }
-    
 }
+

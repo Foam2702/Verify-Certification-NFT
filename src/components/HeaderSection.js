dae4fe0f3dedbc7db4f316fe07b9330e7de7831e
VietPham <20120155@student.hcmus.edu.vn>
2024-06-12 12:21:45 +0700

    request to sign if first login

--- src/components/HeaderSection.js
+++ src/components/HeaderSection.js
@@ -7,6 +7,8 @@ import Typography from '@mui/material/Typography';
 import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
 import Admin from "../pages/Admin";
 import AddressAvatar from "../components/AddressAvatar"
 import CircularProgress from '@mui/material/CircularProgress';
@@ -24,11 +26,15 @@ const settings = [
 
 ]; const HeaderSection = () => {
   const SOULBOUND_ADDRESS = process.env.REACT_APP_SOULBOUND_ADDRESS
-  const { signer, loading, address, connectWallet, getPublicKey } = useSigner();
+  const { signer, address, connectWallet, getPublicKey } = useSigner();
   const [tickets, setTickets] = useState([])
   const [loadingPage, setLoadingPage] = useState(false);
   const [anchorElNav, setanchorElNav] = React.useState(null);
   const [anchorElUser, setanchorElUser] = React.useState(null);
+  const [showAlert, setShowAlert] = useState(false);
+  const [messageAlert, setMessageAlert] = useState("")
+
+  const [alertSeverity, setAlertSeverity] = useState("");
   const navigate = useNavigate();
 
 
@@ -60,6 +66,16 @@ const settings = [
     }
 
   };
+  const handleClose = async (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    // setLoading(true);
+    setShowAlert(false);
+    // await new Promise(resolve => setTimeout(resolve, 1000));
+    // setLoading(false);
+
+  };
   useEffect(() => {
     const fetchTickets = async () => {
       const allTickets = await axios("http://localhost:8080/tickets/all");
@@ -81,6 +97,33 @@ const settings = [
     };
     fetchTickets().catch(error => console.error(error));
   }, [signer, address]);
+  useEffect(() => {
+    const insertPubToDB = async () => {
+      if (address) {
+        const checkPublicKeyExisted = await axios.get(`http://localhost:8080/addresses/${address}`);
+        if (checkPublicKeyExisted.data.address.length === 0) {
+          const publicKey = await getPublicKey(); // Await the result of getPublicKey
+          if (publicKey.code === 4001 && publicKey.message === "User rejected the request.") {
+            console.log('Error retrieving public key:', publicKey);
+            setAlertSeverity("warning");
+            setMessageAlert("Reject to sign");
+            setShowAlert(true);
+            return;
+          }
+          await axios.post(`http://localhost:8080/addresses/${address}`, {
+            address: address, // Include the address in the body
+            publicKey: publicKey // Include the public key in the body
+          });
+          setAlertSeverity("success");
+          setMessageAlert("Sign successfully");
+          setShowAlert(true);
+
+        }
+
+      }
+    };
+    insertPubToDB();
+  }, [address, signer])
 
   const filteredSettings = address === process.env.REACT_APP_ADMIN ? settings : settings.filter(setting => setting.name !== 'Issuer Management');
 
@@ -149,14 +192,24 @@ const settings = [
                 )}
               </Box>
 
-
-
-
               {loadingPage && (
                 <div className="loading-overlay">
                   <CircularProgress />
                 </div>
               )}
+              <Snackbar open={showAlert} autoHideDuration={10000} onClose={handleClose}>
+                <Alert
+                  onClose={handleClose}
+                  severity={alertSeverity}
+                  variant="filled"
+                  sx={{
+                    width: '100%',
+                    fontSize: '1.25rem', // Increase font size
+                  }}
+                >
+                  {messageAlert}
+                </Alert>
+              </Snackbar>
 
             </div>
           </div>

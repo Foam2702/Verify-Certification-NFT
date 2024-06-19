81f03cd4c6301d3f67e3c689baa5600426780046
VietPham <20120155@student.hcmus.edu.vn>
2024-06-19 14:01:09 +0700

    delete upload file to pinata in backend

--- Backend/src/service/uploadImage.js
+++ Backend/src/service/uploadImage.js
@@ -16,41 +16,6 @@ async function deleteFile(image) {
     }
 
 }
-async function imageUpload(image) {
-    try {
-        const imagePath = image.path; // Assuming 'appDir' is defined elsewhere
-        const formData = new FormData();
-        const file = fs.createReadStream(imagePath);
-        const pinataMetadata = JSON.stringify({
-            name: `certificate of ${image.owner}`,
-        });
-        const pinataOptions = JSON.stringify({
-            cidVersion: 1,
-        });
-        formData.append("file", file);
-        formData.append("pinataMetadata", pinataMetadata);
-        formData.append("pinataOptions", pinataOptions);
-        const res = await axios.post(
-            "https://api.pinata.cloud/pinning/pinFileToIPFS",
-            formData,
-            {
-                headers: {
-                    Authorization: `Bearer ${JWT}`,
-                },
-            }
-        );
-        fs.unlink(image.path, (err) => {
-            if (err) {
-                console.error(err);
-            } else {
-            }
-        });
-        return res.data.IpfsHash
-    } catch (error) {
-        console.log(error);
-    }
 
 
-}
-
-module.exports = imageUpload;
+module.exports = deleteFile;

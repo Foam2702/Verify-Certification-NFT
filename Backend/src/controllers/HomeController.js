0399eeeaa96d3c606ace00c6b4a171c2846a8a46
VietPham <20120155@student.hcmus.edu.vn>
2024-07-03 13:25:16 +0700

    delete top3course

--- Backend/src/controllers/HomeController.js
+++ Backend/src/controllers/HomeController.js
@@ -1,12 +1,6 @@
 const courseModel = require("../models/CourseModel")
 
 module.exports = {
-    getTop3Courses: async (req, res) => {
-        const courses = await courseModel.getTop3Courses();
-        res.json({
-            status: 200,
-            courses
-        })
-    }
+
 
 }
--- Backend/src/routes/home.js
+++ Backend/src/routes/home.js
@@ -3,8 +3,6 @@ const HomeController = require("../controllers/HomeController")
 const router = express.Router();
 
 
-router
-    .route("/")
-    .get(HomeController.getTop3Courses)
+
 
 module.exports = router;

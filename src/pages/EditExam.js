13b5e393a2fae3e035341da653fec8e35d446bab
VietPham <20120155@student.hcmus.edu.vn>
2024-07-21 16:27:29 +0700

    single choice in update

--- src/pages/EditExam.js
+++ src/pages/EditExam.js
@@ -625,7 +625,7 @@ const EditExam = () => {
                                             <br></br>
                                             <br></br>
 
-                                            <Typography variant="body2" style={{ color: 'grey' }}>You can add maximum 4 options. If you want to add more then change in settings. Multiple choice single option is availible</Typography>
+                                            <Typography variant="body2" style={{ color: 'grey' }}>You can add maximum 4 options</Typography>
                                             <TextField
                                                 fullWidth={true}
                                                 placeholder="Correct Answer"
